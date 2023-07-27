fd0ba4b36303683edbd7711be352f28138726925
Rakesh Pawar <125602231+RakeshDP@users.noreply.github.com>
2023-07-27 21:18:36 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,41 +1,47 @@
 //complete this code
 class Person {
-	constructor(name, age) {
+	constructor(name, age){
 		this._name = name;
 		this._age = age;
 	}
-
-	// Getter for Name
-	get name() {
-		return this._name;
+	
+	get name(){
+        return this._name;
 	}
-
-	// Setter for Age
-	set age(age) {
-		this._age = age;
+	get age(){
+		return this._age;
+	}
+	set age(newAge){
+		this._age = newAge;
 	}
 }
 
 class Student extends Person {
-	 constructor(name, age) {
-    super(name, age);
-  }
-	study() {
-		console.log(`${this._name} is studying.`);
+	constructor(name, age){
+		super(name, age);
+	}
+	study(){
+		console.log(`${this._name} is studying`);
 	}
 }
 
 class Teacher extends Person {
-	 constructor(name, age) {
-    super(name, age);
-  }
-	teach() {
-		console.log(`${this._name} is teaching.`);
+	constructor(name, age){
+		super(name, age);
+	}
+	teach(){
+		console.log(`${this._name} is teaching`);
 	}
 }
+const person1 = new Person("John", 30);
+console.log(`Person's name: ${person1.name}, Age: ${person1.age}`);
 
+const student1 = new Student("JAlice", 20);
+student1.study();
+
+const teacher1 = new Teacher("Ms.shy", 30);
+teacher1.teach();
 // Do not change the code below this line
 window.Person = Person;
 window.Student = Student;
 window.Teacher = Teacher;
-
