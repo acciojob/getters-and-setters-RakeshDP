eff8a55bd6a804096410043e03861a498d8d74c2
Rakesh Pawar <125602231+RakeshDP@users.noreply.github.com>
2023-07-27 16:50:18 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,9 +1,33 @@
 //complete this code
-class Person {}
+class Person {
+	constructor(name, age){
+		this.name=name;
+		this.age=age;
+	}
+	//Getter for Name
+	get getName(){
+		return this.name;
+	}
+	//Setter for Age
+	set setAge(Newage){
+		 this.age = Newage;
+	}
+	get getAge(){
+		return this.age;
+	}
+}
 
-class Student extends Person {}
 
-class Teacher extends Person {}
+class Student extends Person {
+	super
+	study(){
+		console.log(`${this.name} is studying`)
+	}
+}
+
+class Teacher extends Person {
+	super
+}
 
 // Do not change the code below this line
 window.Person = Person;
