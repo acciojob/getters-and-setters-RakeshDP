e3404bcca9f09579b78fb7e0d101d6a936d73c46
Rakesh Pawar <125602231+RakeshDP@users.noreply.github.com>
2023-07-27 20:49:34 +0530

    created a submission

--- script.js
+++ script.js
@@ -12,26 +12,15 @@ class Person {
 	set setAge(age){
 		 this.age = age;
 	}
-	// get getAge(){
-	// 	return this._age;
-	// }
 }
 
-
 class Student extends Person {
-	constructor(name,age){
-		super(name,age);
-	}
-	
 	study(){
 		console.log(`${this.name} is studying.`)
 	}
 }
 
 class Teacher extends Person {
-	constructor(name,age){
-		super(name,age);
-	}
 	teach(){
 		console.log(`${this.name} is teaching`);
 	}
